64fe9f790042ba1f6523c4367c7292a5ebe0f8ca
Daniel Ignat <daniel.ignat@urbit.com>
2018-10-27 12:56:56 +0200

    Log where state is loaded from

--- src/state/read-state.js
+++ src/state/read-state.js
@@ -1,24 +1,25 @@
-let fs = require('fs')
-let os = require('os')
+let fs = require("fs");
+let os = require("os");
 
-let stateFilePath = os.tmpdir() + '/state.json'
+let stateFilePath = os.tmpdir() + "/state.json";
 
 module.exports = {
-    read
-}
+  read
+};
 
 function read() {
-    return fileExists(stateFilePath) ? 
-        JSON.parse(fs.readFileSync(stateFilePath, 'utf-8')) :
-        {}
+  console.log(`Checking for state.json at ${stateFilePath}`);
+  return fileExists(stateFilePath)
+    ? JSON.parse(fs.readFileSync(stateFilePath, "utf-8"))
+    : {};
 }
 
 function fileExists(filePath) {
-    try {
-        fs.accessSync(filePath, fs.R_OK)    
-    } catch (error) {
-        return false
-    }
-    
-    return true
+  try {
+    fs.accessSync(filePath, fs.R_OK);
+  } catch (error) {
+    return false;
+  }
+
+  return true;
 }
