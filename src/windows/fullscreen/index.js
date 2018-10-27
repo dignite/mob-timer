1a5352250d09253e49eae9838f158d25504e6e45
Daniel Ignat <daniel.ignat@urbit.com>
2018-10-27 13:01:13 +0200

    Run prettier

--- src/windows/fullscreen/index.js
+++ src/windows/fullscreen/index.js
@@ -1,33 +1,33 @@
-const ipc = require('electron').ipcRenderer
+const ipc = require("electron").ipcRenderer;
 
-const skipBtn = document.getElementById('skip')
-const startTurnBtn = document.getElementById('startTurn')
-const configureBtn = document.getElementById('configure')
-const currentEl = document.getElementById('current')
-const currentPicEl = document.getElementById('currentPic')
-const nextEl = document.getElementById('next')
-const nextPicEl = document.getElementById('nextPic')
+const skipBtn = document.getElementById("skip");
+const startTurnBtn = document.getElementById("startTurn");
+const configureBtn = document.getElementById("configure");
+const currentEl = document.getElementById("current");
+const currentPicEl = document.getElementById("currentPic");
+const nextEl = document.getElementById("next");
+const nextPicEl = document.getElementById("nextPic");
 
-ipc.on('rotated', (event, data) => {
+ipc.on("rotated", (event, data) => {
   if (!data.current) {
-    data.current = {name: "Add a mobber"}
+    data.current = { name: "Add a mobber" };
   }
-  currentEl.innerHTML = data.current.name
-  currentPicEl.src = data.current.image || "../img/sad-cyclops.png"
+  currentEl.innerHTML = data.current.name;
+  currentPicEl.src = data.current.image || "../img/sad-cyclops.png";
 
   if (!data.next) {
-    data.next = data.current
+    data.next = data.current;
   }
-  nextEl.innerHTML = data.next.name
-  nextPicEl.src = data.next.image || "../img/sad-cyclops.png"
-})
+  nextEl.innerHTML = data.next.name;
+  nextPicEl.src = data.next.image || "../img/sad-cyclops.png";
+});
 
-ipc.on('configUpdated', (event, data) => {
-  countEl.innerHTML = formatTime(data.secondsPerTurn)
-})
+ipc.on("configUpdated", (event, data) => {
+  countEl.innerHTML = formatTime(data.secondsPerTurn);
+});
 
-skipBtn.addEventListener('click', _ => ipc.send('skip'))
-startTurnBtn.addEventListener('click', _ => ipc.send('startTurn'))
-configureBtn.addEventListener('click', _ => ipc.send('configure'))
+skipBtn.addEventListener("click", _ => ipc.send("skip"));
+startTurnBtn.addEventListener("click", _ => ipc.send("startTurn"));
+configureBtn.addEventListener("click", _ => ipc.send("configure"));
 
-ipc.send('fullscreenWindowReady')
+ipc.send("fullscreenWindowReady");
